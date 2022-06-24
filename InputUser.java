5fd4e364194aee93681ac9fae3e4bc180b2ad998
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-06-24 11:00:19 +0530

    Update InputUser.java

--- InputUser.java
+++ InputUser.java
@@ -1,12 +1,20 @@
+// Java Program for Get input from user
 import java.util.Scanner;
 class InputUser {
     public static void main(String[] args) {
     	
+        // Scanner definition
         Scanner input = new Scanner(System.in);
         String name;	
+        
+        // Asking for input from user
         System.out.print("Enter Your name: ");
+        
+        // Integer reading internally
         name = input.nextLine();
-
+        
+        
+        // Printing name
         System.out.println("My name is: " + name);
 
     }
