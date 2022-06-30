15c7061c18dd66edf37bd9f4971623235e77748b
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-06-30 22:29:10 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,13 +1,18 @@
+/*
+@author: Arnab Kumar Hoodati
+*/
+
+//Draw a Pattern of pyramid
 public class Pyramid {
 	public static void main(String[] args) {
-		int i, j, n=5;
-		for (i=0; i<n; i++)
+		int i, j, n=5;		//Define variable
+		for (i=0; i<n; i++)	//outerloop of rows
 		{
-		for (j=n-i; j>1; j--)
+		for (j=n-i; j>1; j--)	//inner loop of space
 		{
-		    System.out.println("  ");
+		    System.out.println(" ");	//Space Counting
 		}
-		for (j=0; j<i; j++)
+		for (j=0; j<i; j++)	//inner loop of column
 		{
 		    System.out.println("* ");
 		}
