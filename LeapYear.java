fc498b13091d3e727d464bccb7b07284c278cfac
ArnabHoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-06-30 22:45:04 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -8,11 +8,11 @@ public class LeapYear {
     public static void main(String[] args) {
         int year = 2016;        //Define Variable
         boolean leap = false;
-        if (year % 400 == 0)
+        if (year % 4 == 0)
         {
-         if (year % 4 == 0)
+         if (year % 400 == 0)
          {
-         if (year % 100 != 0)
+         if (year % 100 == 0)
              leap = true;
          else
              leap = false;
