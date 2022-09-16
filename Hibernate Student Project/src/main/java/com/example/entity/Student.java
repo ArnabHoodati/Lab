51131c2694fc39601acce46a21730781ae6833ac
Arnab Hoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-09-16 12:50:00 +0530

    Update Student.java

--- Hibernate Student Project/src/main/java/com/example/entity/Student.java
+++ Hibernate Student Project/src/main/java/com/example/entity/Student.java
@@ -1,5 +1,8 @@
 package com.example.entity;
-
+/*
+ *@author Arnab Kumar Hoodati 
+ * 
+ */
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
