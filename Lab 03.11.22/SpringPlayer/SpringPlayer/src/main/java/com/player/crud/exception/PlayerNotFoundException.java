b4bcb76f08268abfdaa6ef3e7dd39cdf401ffd58
Arnab Hoodati <107671204+ArnabHoodati@users.noreply.github.com>
2022-11-04 03:05:37 +0530

    Add files via upload

---
+++ Lab 03.11.22/SpringPlayer/SpringPlayer/src/main/java/com/player/crud/exception/PlayerNotFoundException.java
@@ -0,0 +1,15 @@
+package com.player.crud.exception;
+
+public class PlayerNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PlayerNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
